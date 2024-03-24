07431fc7dcb8f57c2ec19d516cd5272a10eb2070
mikhailyatsenko <misha.ggg@gmail.com>
2024-03-24 21:14:59 +0100

    edit browserRouter for gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./app/App.tsx";
 import "./index.scss";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StoreProvider } from "app/providers/StoreProvider/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StoreProvider>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/bvg-app-ts/"}>
+    {/* <BrowserRouter basename={import.meta.env.DEV ? "/" : "/bvg-app-ts/"}> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </StoreProvider>
 );
