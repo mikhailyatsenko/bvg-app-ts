381b45bef28472a1c020c2c0d6255f6b6b335c64
mikhailyatsenko <misha.ggg@gmail.com>
2024-03-28 23:50:50 +0100

    fix click on fav handler

--- src/features/DisplayFavoritesStops/ui/DisplayFavoritesStops.tsx
+++ src/features/DisplayFavoritesStops/ui/DisplayFavoritesStops.tsx
@@ -2,14 +2,20 @@ import { ButtonWithFavStop } from "entities/ButtonWithFavStop";
 import { useSelector } from "react-redux";
 import { getFavoritesStops } from "features/AddToFavorites/model/selectors/getFavoritesStops/getFavoritesStops";
 import cls from "./DisplayFavoritesStops.module.scss";
+import { useNavigate } from "react-router-dom";
+import { type Stop } from "features/Stops";
 
 export const DisplayFavoritesStops = () => {
   const favoritesStops = useSelector(getFavoritesStops);
+  const navigate = useNavigate();
+  const selectStopHandler = (selectedStop: Stop): void => {
+    navigate(`/arrivals?id=${selectedStop.id}`, { state: { stopName: selectedStop.name } });
+  };
   return (
     <div className={cls.DisplayFavoritesStops}>
       {favoritesStops.length
         ? favoritesStops.map((stop) => (
-            <ButtonWithFavStop isFav={true} key={stop.id} selectStopHandler={console.log} stop={stop} />
+            <ButtonWithFavStop isFav={true} key={stop.id} selectStopHandler={selectStopHandler} stop={stop} />
           ))
         : ""}
     </div>
