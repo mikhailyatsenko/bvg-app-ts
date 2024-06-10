d5584d55350017f2d7574c5d85ff1606701cd879
mikhailyatsenko <misha.ggg@gmail.com>
2024-06-10 14:44:35 +0200

    fix arrivals page height

--- src/pages/ArrivalsPage/ui/ArrivalsPage.tsx
+++ src/pages/ArrivalsPage/ui/ArrivalsPage.tsx
@@ -2,15 +2,16 @@ import { AddToFavorites } from "features/AddToFavorites";
 import { BackToSearch } from "features/BackToSearch";
 import { LoadArrivals } from "features/LoadArrivals";
 import { FiltersPanel } from "widgets/FiltersPanel";
+import cls from "./ArrivalsPage.module.scss";
 
 const ArrivalsPage = () => {
   return (
-    <>
+    <div className={cls.ArrivalsPage}>
       <FiltersPanel />
       <BackToSearch />
       <AddToFavorites />
       <LoadArrivals />
-    </>
+    </div>
   );
 };
 
