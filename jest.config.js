5308807c76c042a39f6fd0fc98e9e8b966a791c1
mikhailyatsenko <misha.ggg@gmail.com>
2024-06-10 14:40:17 +0200

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 export default {
-    testEnvironment: "jsdom",
-    transform: {
-      "^.+\\.tsx?$": "ts-jest",
-    },
-  
-    moduleNameMapper: {
-      "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-      "^.+\\.svg$": "jest-transformer-svg",
-      '\\.(css|less)$': 'identity-obj-proxy',
-    },
-  
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  };
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.svg$": "jest-transformer-svg",
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
+
+  moduleDirectories: ["node_modules", "<rootDir>/src"],
+
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+  rootDir: "./",
+};
